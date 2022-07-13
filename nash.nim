cec24d987eeab33be384d82cef432a0748c2d2e8
Tuatarian <betterthangeowizard@gmail.com>
2022-07-13 19:18:58 +0530

    funcs for pseudo-link detection, removal of 3cycle non-strong pseudos, and filling strong pseudos

--- nash.nim
+++ nash.nim
@@ -1,4 +1,4 @@
-import raylib, jnhex, zero_functional, sequtils, rayutils, tables, heapqueue, lenientops, sugar, algorithm, random
+import raylib, jnhex, zero_functional, sequtils, rayutils, tables, heapqueue, lenientops, sugar, algorithm, random, std/enumerate
 
 randomize()
 
@@ -76,7 +76,12 @@ func pseudoLocs(h : int, b : Board) : seq[int] =
         pos + makevec2(1, 1),
         pos - makevec2(1, 1)].filter(x => x in makerect(makevec2(0, 0), makevec2(12, 12))).map(x => posToInd x)
 
-func findStrongPseudos(b : Board) : seq[(int, int)] =
+func checkPseudo(h, h1 : int, b : Board) : bool =
+    let hexList = b.hexes
+    let (iAdj, jAdj) = (getAdj(h, b), getAdj(h1, b))
+    return iAdj.filter(x => x in jAdj and hexList[x].stone == NONE).len == 2
+
+func findPseudos(b : Board) : seq[(int, int)] =
     let hexList = b.hexes
     let wStones = hexList.filter(x => x.stone == WH).map(x => x.pos)
     let bStones = hexList.filter(x => x.stone == BL).map(x => x.pos)
@@ -86,7 +91,7 @@ func findStrongPseudos(b : Board) : seq[(int, int)] =
             if j in iPseu:
                 let iAdj = getAdj(i, b)
                 let jAdj = getAdj(j, b)
-                if iAdj.filter(x => x in jAdj and hexList[x].stone == NONE).len == 2:
+                if checkPseudo(i, j, b):
                     result.add (i, j)
     for i in 0..<bStones.len - 1:
         let iPseu = pseudoLocs(i, b)
@@ -94,9 +99,34 @@ func findStrongPseudos(b : Board) : seq[(int, int)] =
             if j in iPseu:
                 let iAdj = getAdj(i, b)
                 let jAdj = getAdj(j, b)
-                if iAdj.filter(x => x in jAdj and hexList[x].stone == NONE).len == 2:
+                if checkPseudo(i, j, b):
                     result.add (i, j)
 
+func remove3cycPseudos(r : seq[(int, int)], b : Board) : seq[(int, int)] =
+    result = r
+    var markedForDel : set[uint8]
+    for i in 0..<result.len - 1:
+        let (p1, p2) = (result[i][0], result[i][1])
+        for j in i+1..<result.len:
+            if p1 in result[j]:
+                let (r1, r2) = (result[j][0], result[j][1])
+                if p1 == r1:
+                    if p2.checkPseudo(r2, b):
+                        markedForDel.incl uint8 r2
+                elif p1 == r2:
+                    if p2.checkPseudo(r1, b):
+                        markedForDel.incl uint8 r2
+    for inx, d in enumerate(markedForDel):
+        result.delete(d.int - inx)
+
+proc fillPseudos(pseudos : seq[(int, int)], b : Board) : Board =
+    let hexList = b.hexes
+    result = b
+    for i in 0..<pseudos.len:
+        let l1 = pseudos[i][0]
+        let l2 = pseudos[i][1]
+        let l1Adj = getAdj(l1, b)
+        result.hexes[getAdj(l2, b).filter(x => x in l1Adj)[roundToInt(rand 1f)]].stone = hexList[l1].stone
 
 func evald0(b : Board) : float =
     let hexList = b.hexes
