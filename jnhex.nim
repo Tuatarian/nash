90443538cea0beda92131d4b0de85808154df38b
Tuatarian <betterthangeowizard@gmail.com>
2022-07-31 07:26:43 -0700

    hFen system among other things

--- jnhex.nim
+++ jnhex.nim
@@ -1,7 +1,9 @@
-import raylib, rayutils, math, sugar, sequtils, zero_functional
+import raylib, rayutils, math, sugar, sequtils, zero_functional, strutils
 
 type Stone* = enum
-    BL, WH, NONE
+    BL = "b", WH = "w", NONE = "n"
+
+const stArr* = [false : BL,true :  WH]
 
 type Hex* = object
     pos* : Vector2
@@ -11,15 +13,39 @@ type Hex* = object
 
 type Board* = object
     hexes* : seq[Hex]
-
-var 
+var
     board : Board
 
-func getEmptyBoard*() : Board = 
+func getEmptyBoard*() : Board =
     for i in 0..12:
         for j in 0..12:
             result.hexes.add Hex(pos : makevec2(i, j), inrad : 35, circumrad : (35 * 2) / sqrt(3f), stone : None)
 board = getEmptyBoard()
+
+func hFen*(b : Board) : string =
+    let hexList = b.hexes
+    var count : int
+    for i in 0..<hexList.len:
+        if hexList[i].stone == NONE:
+            count += 1
+        else:
+            result &= $count & $hexList[i].stone
+            count = 0
+
+func loadHFen*(inp : string) : Board =
+    result = getEmptyBoard()
+    var shift : string
+    var cInx : int
+    for c in inp:
+        if c in '0'..'9':
+            shift &= c
+        else:
+            cInx += shift.parseInt + 1
+            if c == 'w':
+                result.hexes[cInx - 1].stone = WH
+            else:
+                result.hexes[cInx - 1].stone = BL
+            shift = ""
 
 proc col(s : Stone) : Color =
     if s == WH:
@@ -36,9 +62,8 @@ proc `[]`*(s : openArray[Hex], v : Vector2) : Hex = s[posToInd v]
 
 proc `[]=`*(s : var openArray[Hex], v : Vector2, h : Hex) = s[v] = h
 
-func getAdj*(ind : int, board : Board) : seq[int] = 
-    let origin = board.hexes[ind]
-    let opos = origin.pos
+proc getAdj*(ind : int, b : Board = board) : seq[int] =
+    let opos = b.hexes[ind].pos
     return @[opos + makevec2(1, -1),
              opos + makevec2(0, -1),
              opos + makevec2(-1, 0),
@@ -49,7 +74,7 @@ func getAdj*(ind : int, board : Board) : seq[int] =
 
 func ind*(h : Hex) : int = return h.pos.posToInd
 
-func checkVictory*(b : Board, ind : int) : bool =
+proc checkVictory*(b : Board, ind : int) : bool =
     var traversed = @[ind]
     var branches = @[ind]
     var nextBranches : seq[int]
@@ -68,7 +93,7 @@ func checkVictory*(b : Board, ind : int) : bool =
                     if res[0]:
                         return true
             for i in 0..<branches.len:
-                nextBranches &= branches[i].getAdj(b).filter(y => y notin traversed and b.hexes[y].col == col)
+                nextBranches &= branches[i].getAdj.filter(y => y notin traversed and b.hexes[y].col == col)
             branches = nextBranches
             nextBranches = @[]
     else:
